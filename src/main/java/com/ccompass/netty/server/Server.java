3dd51b8284b531931435b9bd032831796b0420e4
jsalbert <jsalbert@itcom888.com>
2019-10-10 15:54:43 +0800

    add maven support run on localhost demo

---
+++ src/main/java/com/ccompass/netty/server/Server.java
@@ -0,0 +1,53 @@
+package com.ccompass.netty.server;
+
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Server {
+
+    /**
+     * 服务端监听的端口地址
+     */
+
+    @Parameter(names = {"--port", "-p"})
+    private static int portNumber = 7878;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Server main = new Server();
+
+        JCommander.newBuilder()
+                .addObject(main)
+                .build()
+                .parse(args);
+
+        log.info(String.valueOf(portNumber));
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new ServerInitializer());
+
+            // 服务器绑定端口监听
+            ChannelFuture f = b.bind(portNumber).sync();
+            System.out.println("服务器启动成功：监听端口：" + portNumber);
+            // 监听服务器关闭监听
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
